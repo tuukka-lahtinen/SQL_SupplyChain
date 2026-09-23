7a21b720bb8ce0f08c0ae30695a36e76e6d3a0c8
Tuukka Lahtinen <tuukka.j.lahtinen@aalto.fi>
2026-09-23 08:29:20 +0300

    Remove outdated README reference in comment

--- sql/04_monthly_sales_trend.sql
+++ sql/04_monthly_sales_trend.sql
@@ -2,7 +2,7 @@
 -- Rajattu 2017-09 asti: loka 2017 - tammi 2018 näyttää äkillisen,
 -- tasomaisen pudotuksen tilausmäärässä (n. 5200 -> 2100/kk),
 -- todennäköisesti datan keräyksen katkos eikä aito myynnin lasku.
--- Ks. README kohta "Data quality notes".
+
 WITH monthly AS (
     SELECT
         DATE_TRUNC('month', order_date)::DATE AS month,
